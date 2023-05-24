924dda64c6ea641e26ff274b81d4a86edb343057
andrea <andreapaola2203@gmail.com>
2023-05-23 23:43:28 -0300

    Ultima actualización
    Cambio de colores.

---
+++ js/script.js
@@ -0,0 +1,31 @@
+document.getElementById("idnav").innerHTML = `
+<div>
+            <div class="logo">
+                <a href="index.html" >
+                    <img src="img/logoHuellas.png" alt="Logo Huellas" width="100px">
+                </a>
+            </div>
+            <div class="nombreLogo">
+                <h4>Consejos<br>para tu Mascota</h4>
+            </div>
+            </div>
+            <div >
+            <ul class="navMenu">
+                <li class="navLista"><a href="index.html">INICIO</a></li>
+                <li class="navLista"><a href="#bannerMascotas"> MASCOTAS </a></li>
+                <li class="navLista"><a href="veterinarias.html" target="_blank">VETERINARIAS</a></li>
+                <li class="navLista"><a href="contacto.html" target="_blank">CONTACTO</a></li>
+            </ul>
+</div>
+`
+
+document.getElementById("idfooter").innerHTML = `
+<div class="contenedorFooter">
+            <div class="footerIntegrantes">
+                <p>Diseñado por: Alejandro Cruz,Andrea Pilipiw, Daniela Tamini </p>
+            </div>
+            <div class="footerRedes">
+                <p>© Copyright 2023 </p>
+            </div>
+            </div>
+`
